030938673aafaf7b9b951552bff174b1d846ddd3
Farhan Agigi <88557261+Farhanagigi@users.noreply.github.com>
2022-12-06 12:31:16 +0200

    Criacao de formulario de cadastro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <BanNer />
   <div class="main-container">
     <h1>Monte o seu Burger:</h1>
-    <p>Formulario</p>
+    <FormuLario />
   </div>
 </template>
 
 <script>
 import { defineComponent } from "@vue/runtime-core";
 import BanNer from "../components/BanNer.vue";
+import FormuLario from "../components/FormuLario.vue";
+
 export default defineComponent({
   name: "HomeView",
   components: {
     BanNer,
+    FormuLario,
   },
 });
 </script>
